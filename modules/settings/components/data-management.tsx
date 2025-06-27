0a2248fe4dc28338b6720de1a643487292783604
AkaraChen <akarachen@outlook.com>
2025-06-27 14:33:31 +0800

    refactor: simplify data management UI by removing Paper wrapper and unused components

--- modules/settings/components/data-management.tsx
+++ modules/settings/components/data-management.tsx
@@ -1,15 +1,12 @@
 import * as React from 'react'
 import { useState } from 'react'
-import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
-import Paper from '@mui/material/Paper'
 import Stack from '@mui/material/Stack'
 import Alert from '@mui/material/Alert'
 import TextField from '@mui/material/TextField'
 import CloudDownloadIcon from '@mui/icons-material/CloudDownload'
 import CloudUploadIcon from '@mui/icons-material/CloudUpload'
-import Divider from '@mui/material/Divider'
 import GLOBAL_STORE from '@/modules/universal/utils/kv-store'
 import { useNotification } from '@/modules/universal/components/notification-provider'
 
@@ -73,88 +70,76 @@ export function DataManagement() {
     }
 
     return (
-        <>
-            <Paper sx={{ p: 3, mb: 3 }}>
-                <Typography variant='h6' gutterBottom>
-                    数据备份
-                </Typography>
-                <Divider sx={{ mb: 2 }} />
-                <Stack spacing={2}>
-                    <Box>
-                        <Button
-                            variant='contained'
-                            onClick={handleBackup}
-                            sx={{ mr: 2 }}
-                        >
-                            生成备份
-                        </Button>
-                        <Button
-                            variant='outlined'
-                            startIcon={<CloudDownloadIcon />}
-                            onClick={handleDownloadBackup}
-                            disabled={!backupData}
-                        >
-                            下载备份文件
-                        </Button>
-                    </Box>
-                    {backupData && (
-                        <TextField
-                            label='备份数据'
-                            multiline
-                            rows={6}
-                            value={backupData}
-                            fullWidth
-                            InputProps={{
-                                readOnly: true,
-                            }}
-                        />
-                    )}
-                </Stack>
-            </Paper>
+        <Stack spacing={2} sx={{ py: 3 }}>
+            <Box>
+                <Button
+                    variant='contained'
+                    onClick={handleBackup}
+                    sx={{ mr: 2 }}
+                >
+                    生成备份
+                </Button>
+                <Button
+                    variant='outlined'
+                    startIcon={<CloudDownloadIcon />}
+                    onClick={handleDownloadBackup}
+                    disabled={!backupData}
+                >
+                    下载备份文件
+                </Button>
+            </Box>
+            {backupData && (
+                <TextField
+                    label='备份数据'
+                    multiline
+                    rows={6}
+                    value={backupData}
+                    fullWidth
+                    slotProps={{
+                        input: {
+                            readOnly: true,
+                        },
+                    }}
+                />
+            )}
 
-            <Paper sx={{ p: 3 }}>
-                <Typography variant='h6' gutterBottom>
-                    数据恢复
-                </Typography>
-                <Divider sx={{ mb: 2 }} />
-                <Stack spacing={2}>
-                    <Box>
-                        <Button
-                            variant='contained'
-                            component='label'
-                            startIcon={<CloudUploadIcon />}
-                            sx={{ mr: 2 }}
-                        >
-                            上传备份文件
-                            <input
-                                type='file'
-                                hidden
-                                accept='.json'
-                                onChange={handleFileUpload}
-                            />
-                        </Button>
-                        <Button
-                            variant='outlined'
-                            onClick={handleRestore}
-                            disabled={!restoreData}
-                        >
-                            恢复数据
-                        </Button>
-                    </Box>
-                    <TextField
-                        label='恢复数据'
-                        multiline
-                        rows={6}
-                        value={restoreData}
-                        onChange={e => setRestoreData(e.target.value)}
-                        fullWidth
-                        placeholder='粘贴备份数据或上传备份文件'
-                    />
-                    <Alert severity='warning'>
-                        警告：恢复操作将覆盖当前所有数据，请确保您有正确的备份文件。
-                    </Alert>
-                </Stack>
-            </Paper>
-        </>
+            <Stack spacing={2}>
+                <Box>
+                    <Button
+                        variant='contained'
+                        component='label'
+                        startIcon={<CloudUploadIcon />}
+                        sx={{ mr: 2 }}
+                    >
+                        上传备份文件
+                        <input
+                            type='file'
+                            hidden
+                            accept='.json'
+                            onChange={handleFileUpload}
+                        />
+                    </Button>
+                    <Button
+                        variant='outlined'
+                        onClick={handleRestore}
+                        disabled={!restoreData}
+                    >
+                        恢复数据
+                    </Button>
+                </Box>
+                <TextField
+                    label='恢复数据'
+                    multiline
+                    rows={6}
+                    value={restoreData}
+                    onChange={e => setRestoreData(e.target.value)}
+                    fullWidth
+                    placeholder='粘贴备份数据或上传备份文件'
+                />
+                <Alert severity='warning'>
+                    警告：恢复操作将覆盖当前所有数据，请确保您有正确的备份文件。
+                </Alert>
+            </Stack>
+        </Stack>
     )
 }
